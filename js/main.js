29fa9dabc8f944d5ea535fd779aa3e1f877850e3
Michael <mpl2999@rit.edu>
2016-05-08 19:20:25 -0400

    Inital Commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	console.log("Hello! test");
+	
+	/* affix the navbar after scroll below header */
+    $(".navbar").affix({offset: {top: $(".top").outerHeight(true)} });
+	
+	$(".scrollToTop").click(function() {
+		console.log("Button pressed");
+		$('html,body').animate({
+			scrollTop: $(".content").offset().top},
+			'slow');
+	});
+});
+
+// initialize audio js
+audiojs.events.ready(function() {
+    var as = audiojs.createAll();
+});
+
